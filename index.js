2b96d62c33cc1aea9296e1eadddd36c466417b0e
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-02 17:32:51 -0300

    more routing and use faker

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const faker = require('faker')
+// const Faker = require('faker/lib')
 
 app.get('/home', (req, res) => {
   res.send("<h1>My Server in Express //  HOME  </h1>")
@@ -13,12 +14,69 @@ app.get('/categories', (req, res) => {
 })
 
 
-app.get('/products', (req, res) => {
+app.get("/categories/:categoryid/products/:productsid", (req,res) => {
+  const { categoryid, productsid } = req.params;
   res.json({
+    categoryid,
+    productsid
+  })
+})
+
+
+app.get('/products', (req, res) => {
+  let products = []
+
+  const {size} = req.query;
+
+  const limit = size || 10
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products)
+  // res.json([ {
+  //   name: "Product One",
+  //   price: "$15.350",
+  //   text: "Responding in json format"
+  // },
+  // {
+  //   name: "Product Two",
+  //   price: "$20.350",
+  //   text: "Product description"
+  // }])
+})
+app.get('/products/filter', (req, res) => {
+  res.send('FILTER')
+})
+
+
+
+app.get('/products/:id', (req,res)=> {
+  const {id} = req.params
+  res.json({
+    id,
     name: "Product One",
     price: "$15.350",
     text: "Responding in json format"
   })
+})
+
+
+
+app.get('/users', (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('<h1>the are not parameters</h1>')
+  }
 })
 
 

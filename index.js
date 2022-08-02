f229d3625a22f3ab1180c2d68636f5a6d4658285
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-02 11:54:36 -0300

    My Server in express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Activo");
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send("My Server in Express")
+})
+
+
+app.listen(port, () => {
+  console.log(('Mi Port' +" "+ port));
+})
+

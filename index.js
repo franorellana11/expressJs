dbd4370dcfb9701f7d0e72ba807727ca6b38b0da
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-02 13:22:24 -0300

    Routing

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => {
-  res.send("My Server in Express")
+app.get('/home', (req, res) => {
+  res.send("<h1>My Server in Express //  HOME  </h1>")
 })
+
+
+app.get('/categories', (req, res) => {
+  res.send("<h1>New Endpoint/Categories</h1>")
+})
+
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: "Product One",
+    price: "$15.350",
+    text: "Responding in json format"
+  })
+})
+
 
 
 app.listen(port, () => {
   console.log(('Mi Port' +" "+ port));
 })
-

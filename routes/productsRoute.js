c09b178d77a354ce2791a25741bab5e6bb276ad6
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-04 19:24:51 -0300

    error handling

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -13,8 +13,8 @@ const service = new ProductsService()
 
 
 
-router.get('/', (req, res) => {
-  const products = service.find()
+router.get('/', async (req, res) => {
+  const products = await service.find()
   res.json(products)
 })
 
@@ -33,30 +33,40 @@ router.get('/', (req, res) => {
 
 
 
-router.get('/:id', (req,res)=> {
+router.get('/:id', async (req,res)=> {
   const {id} = req.params
-  const product = service.findOne(id)
+  const product = await service.findOne(id)
   res.json(product)
 })
 
 ///////////////////////////////////// POST METHOD ///////////////////
 
-router.post('/', (req, res)=> {
+router.post('/', async (req, res)=> {
   const body = req.body
-  const newProduct = service.create(body)
+  const newProduct = await service.create(body)
   res.status(201).json({newProduct})
 })
 
 
 ///////////////////////////////// PATCH METHOD /////////////////////////
 
-router.patch('/:id', (req, res)=> {
-  const { id } = req.params
-  const body = req.body
-  const product = service.update(id,body)
-  res.json({
-    product
-  })
+router.patch('/:id', async (req, res)=> {
+  try {
+    const { id } = req.params
+    const body = req.body
+    const product = await service.update(id,body)
+    res.json({
+      product
+    })
+  } catch (error) {
+    res.status(404).json({
+      message: "NOT FOUND"
+    })
+  }
+
+
+
+
 })
 
 
@@ -64,14 +74,11 @@ router.patch('/:id', (req, res)=> {
 
 
 
-router.delete('/:id', (req, res)=> {
+router.delete('/:id', async (req, res)=> {
   const { id } = req.params
-  const rta = service.delete(id)
+  const rta = await service.delete(id)
   res.json({rta})
 })
-
-
-
 
 
 
--- services/productsService.js
+++ services/productsService.js
@@ -23,7 +23,7 @@ class ProductsService {
   }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -32,15 +32,20 @@ class ProductsService {
     return newProduct
   }
 
-  find() {
-    return this.products
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products)
+      }, 5000);
+    })
+    // return this.products
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id)
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id)
     if (index === -1) {
       throw new Error('Product not found')
@@ -53,7 +58,7 @@ class ProductsService {
     return this.products[index]
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id)
     if (index === -1) {
       throw new Error('Product not found')

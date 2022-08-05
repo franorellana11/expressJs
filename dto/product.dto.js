aa8020b914f7223dae959173d41a73823fd32003
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-04 23:31:53 -0300

    validation of information

---
+++ dto/product.dto.js
@@ -0,0 +1,26 @@
+/// Validacion de informacion
+const Joi = require('joi')
+
+const id = Joi.string().uuid()
+
+const name = Joi.string().alphanum().min(3).max(25)
+
+const price = Joi.number().integer().min(20)
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required()
+})
+
+const updateProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required()
+})
+
+
+const getProductSchema = Joi.object({
+  id: id.required()
+})
+
+
+module.exports = {createProductSchema,getProductSchema,updateProductSchema}
